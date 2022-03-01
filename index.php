9322611e6c58ada841b0494afd21380adb4745ec
alifpramanarasa10 <89970749+alifpramanarasa10@users.noreply.github.com>
2022-03-01 20:50:09 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar BE</title>
+</head>
+<body>
+    <?php 
+        $first = 1;
+        $second = 3;
+
+        echo "Hasil dari penjumlahan $first + $second adalah ".($first+$second);
+
+    ?>
+</body>
+</html>
